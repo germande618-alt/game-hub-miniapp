9078dff44d4ecf1bdb689f13e6bd8af9799c4eff
germande618-alt <germande618@gmail.com>
2026-03-10 15:20:18 +0200

    Update cards.js

--- games/couple/questions/cards.js
+++ games/couple/questions/cards.js
@@ -1,1 +1,25 @@
+const coupleQuestions = {
 
+ru:[
+"Как вы познакомились?",
+"Что тебе больше всего нравится в партнере?",
+"Куда вы хотите поехать вместе?",
+"Какой самый романтичный момент у вас был?"
+],
+
+en:[
+"How did you meet?",
+"What do you like most about your partner?",
+"Where would you like to travel together?",
+"What was your most romantic moment?"
+],
+
+es:[
+"¿Cómo se conocieron?"
+],
+
+fr:[
+"Comment vous êtes-vous rencontrés ?"
+]
+
+}
