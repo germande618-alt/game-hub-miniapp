6b3250abc4155ed1ef1fc0787d81978b9ef47b0d
germande618-alt <germande618@gmail.com>
2026-03-10 15:36:46 +0200

    Update cards.js

--- games/party/never/cards.js
+++ games/party/never/cards.js
@@ -1,1 +1,25 @@
+const neverCards = {
 
+ru:[
+"Я никогда не влюблялся с первого взгляда",
+"Я никогда не врал своим друзьям",
+"Я никогда не пел в душе",
+"Я никогда не опаздывал на важную встречу"
+],
+
+en:[
+"I have never fallen in love at first sight",
+"I have never lied to my friends",
+"I have never sung in the shower",
+"I have never been late for something important"
+],
+
+es:[
+"Nunca me he enamorado a primera vista"
+],
+
+fr:[
+"Je ne suis jamais tombé amoureux au premier regard"
+]
+
+}
