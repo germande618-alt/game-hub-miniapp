2fe3bf6edb3da001a491519b6dfb0f95a19f1593
germande618-alt <germande618@gmail.com>
2026-03-10 16:00:39 +0200

    Update cards.js

--- games/party/likely/cards.js
+++ games/party/likely/cards.js
@@ -1,1 +1,25 @@
+const likelyCards = {
 
+ru:[
+"Кто скорее всего напишет бывшему ночью?",
+"Кто скорее всего станет миллионером?",
+"Кто скорее всего уснет на вечеринке?",
+"Кто скорее всего забудет день рождения друга?"
+],
+
+en:[
+"Who is most likely to text their ex at night?",
+"Who is most likely to become a millionaire?",
+"Who is most likely to fall asleep at a party?",
+"Who is most likely to forget a friend's birthday?"
+],
+
+es:[
+"¿Quién es más probable que escriba a su ex por la noche?"
+],
+
+fr:[
+"Qui est le plus susceptible d'écrire à son ex la nuit ?"
+]
+
+}
