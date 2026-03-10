b9fda76e90f00607052e9afe7d429bad7b5768aa
germande618-alt <germande618@gmail.com>
2026-03-10 15:20:47 +0200

    Update game.js

--- games/couple/questions/game.js
+++ games/couple/questions/game.js
@@ -1,1 +1,24 @@
+function startCoupleQuestions(){
 
+const list = coupleQuestions[lang] || coupleQuestions.en
+const card = list[Math.floor(Math.random()*list.length)]
+
+showCoupleQuestion(card)
+
+}
+
+function showCoupleQuestion(text){
+
+const t = translations[lang]
+
+document.getElementById("app").innerHTML = `
+
+<h2>${text}</h2>
+
+<button onclick="startCoupleQuestions()">${t.next}</button>
+
+<button onclick="openCouple()">${t.back}</button>
+
+`
+
+}
