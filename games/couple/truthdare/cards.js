a713de4e30ea8f1acc060507e55eb9fe00bc5398
germande618-alt <germande618@gmail.com>
2026-03-10 14:30:35 +0200

    Update cards.js

--- games/couple/truthdare/cards.js
+++ games/couple/truthdare/cards.js
@@ -1,1 +1,35 @@
+const cards = {
 
+truth:{
+
+ru:[
+"Чего ты боишься больше всего?",
+"Кто тебе нравится больше всего здесь?",
+"Какой самый неловкий момент в твоей жизни?"
+],
+
+en:[
+"What is your biggest fear?",
+"Who do you like the most here?",
+"What was your most embarrassing moment?"
+]
+
+},
+
+dare:{
+
+ru:[
+"Танцуй 10 секунд",
+"Спой песню",
+"Сделай 5 отжиманий"
+],
+
+en:[
+"Dance for 10 seconds",
+"Sing a song",
+"Do 5 pushups"
+]
+
+}
+
+}
