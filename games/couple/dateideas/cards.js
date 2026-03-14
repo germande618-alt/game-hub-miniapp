82527241162ebc45105bcbc18b89a5588b798d48
germande618-alt <germande618@gmail.com>
2026-03-14 22:14:57 +0200

    Update cards.js

--- games/couple/dateideas/cards.js
+++ games/couple/dateideas/cards.js
@@ -1,49 +1,173 @@
-const homeIdeas = [
+const ideas = {
 
+home:{
+ru:[
 "Приготовьте ужин вместе",
 "Устройте вечер настольных игр",
 "Посмотрите фильм под пледом",
-"Сделайте домашнюю пиццу",
-"Приготовьте десерт вместе"
+"Приготовьте десерт вместе",
+"Сделайте домашнюю пиццу"
+],
 
+en:[
+"Cook dinner together",
+"Have a board game night",
+"Watch a movie under a blanket",
+"Make dessert together",
+"Make homemade pizza"
+],
+
+es:[
+"Cocinen juntos",
+"Noche de juegos de mesa",
+"Ver una película bajo una manta",
+"Preparar postre juntos",
+"Hacer pizza casera"
+],
+
+fr:[
+"Cuisinez ensemble",
+"Soirée jeux de société",
+"Regarder un film sous un plaid",
+"Préparer un dessert ensemble",
+"Faire une pizza maison"
 ]
+},
 
-const walkIdeas = [
-
+walk:{
+ru:[
 "Прогуляйтесь в новом районе",
 "Сходите на пикник",
 "Покатайтесь на велосипедах",
-"Погуляйте в парке ночью",
-"Посмотрите закат вместе"
+"Посмотрите закат",
+"Прогуляйтесь по ночному городу"
+],
 
+en:[
+"Walk in a new neighborhood",
+"Go on a picnic",
+"Ride bicycles together",
+"Watch the sunset",
+"Walk through the city at night"
+],
+
+es:[
+"Pasear por un barrio nuevo",
+"Ir de picnic",
+"Andar en bicicleta juntos",
+"Ver la puesta de sol",
+"Pasear por la ciudad de noche"
+],
+
+fr:[
+"Promenez-vous dans un nouveau quartier",
+"Faire un pique-nique",
+"Faire du vélo ensemble",
+"Regarder le coucher du soleil",
+"Se promener la nuit en ville"
 ]
+},
 
-const cultureIdeas = [
-
+culture:{
+ru:[
 "Сходите в музей",
 "Посетите театр",
 "Сходите на концерт",
-"Посмотрите выставку",
-"Сходите в галерею"
+"Посетите выставку",
+"Сходите в художественную галерею"
+],
 
+en:[
+"Visit a museum",
+"Go to the theater",
+"Attend a concert",
+"Visit an exhibition",
+"Go to an art gallery"
+],
+
+es:[
+"Visitar un museo",
+"Ir al teatro",
+"Asistir a un concierto",
+"Visitar una exposición",
+"Ir a una galería de arte"
+],
+
+fr:[
+"Visiter un musée",
+"Aller au théâtre",
+"Assister à un concert",
+"Visiter une exposition",
+"Aller dans une galerie d'art"
 ]
+},
 
-const extremeIdeas = [
-
+extreme:{
+ru:[
 "Покатайтесь на картинге",
 "Попробуйте скалолазание",
 "Катание на квадроциклах",
-"Прыжок с тарзанки",
-"Верёвочный парк"
+"Верёвочный парк",
+"Попробуйте квест-комнату"
+],
 
+en:[
+"Go kart racing",
+"Try rock climbing",
+"Ride quad bikes",
+"Visit a rope park",
+"Try an escape room"
+],
+
+es:[
+"Correr en karting",
+"Probar escalada",
+"Pasear en quad",
+"Parque de cuerdas",
+"Probar escape room"
+],
+
+fr:[
+"Faire du karting",
+"Essayer l'escalade",
+"Faire du quad",
+"Parc d'accrobranche",
+"Escape game"
 ]
+},
 
-const romanticIdeas = [
-
+romantic:{
+ru:[
 "Свидание при свечах",
 "Напишите друг другу любовные письма",
-"Сделайте совместную фотосессию",
 "Посмотрите звезды",
-"Сделайте друг другу сюрприз"
+"Сделайте совместную фотосессию",
+"Устройте романтический ужин"
+],
 
+en:[
+"Candlelight dinner",
+"Write love letters to each other",
+"Watch the stars",
+"Have a couple photoshoot",
+"Have a romantic dinner"
+],
+
+es:[
+"Cena a la luz de las velas",
+"Escribirse cartas de amor",
+"Mirar las estrellas",
+"Hacer una sesión de fotos",
+"Cena romántica"
+],
+
+fr:[
+"Dîner aux chandelles",
+"Écrire des lettres d'amour",
+"Regarder les étoiles",
+"Faire une séance photo",
+"Dîner romantique"
 ]
+}
+
+}
