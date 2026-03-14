dad9bc0d0460bbc2e07ae0c59ce0073a470d10e3
germande618-alt <germande618@gmail.com>
2026-03-14 16:11:00 +0200

    Update adult_cards.js

--- games/couple/truthdare/adult_cards.js
+++ games/couple/truthdare/adult_cards.js
@@ -1,7 +1,39 @@
-const adultTruth = [
-"Какая у тебя романтическая фантазия?"
+const adultTruth = {
+
+ru:[
+"Какая у тебя самая смелая фантазия?"
+],
+
+en:[
+"What is your boldest fantasy?"
+],
+
+es:[
+"¿Cuál es tu fantasía más atrevida?"
+],
+
+fr:[
+"Quelle est ta fantaisie la plus audacieuse ?"
 ]
 
-const adultDare = [
-"Поцелуй меня"
+}
+
+const adultDare = {
+
+ru:[
+"Поцелуй партнёра страстно"
+],
+
+en:[
+"Kiss your partner passionately"
+],
+
+es:[
+"Besa apasionadamente a tu pareja"
+],
+
+fr:[
+"Embrasse ton partenaire passionnément"
 ]
+
+}
