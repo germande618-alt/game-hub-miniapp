caacb20229d887f03099aaa411c4cec2adfeaf97
germande618-alt <germande618@gmail.com>
2026-03-14 16:06:25 +0200

    Update date_cards.js

--- games/couple/truthdare/date_cards.js
+++ games/couple/truthdare/date_cards.js
@@ -1,9 +1,47 @@
-const dateTruth = [
+const dateTruth = {
+
+ru:[
 "Какое было первое впечатление обо мне?",
 "Ты волновался перед встречей?"
+],
+
+en:[
+"What was your first impression of me?",
+"Were you nervous before the date?"
+],
+
+es:[
+"¿Cuál fue tu primera impresión de mí?",
+"¿Estabas nervioso antes de la cita?"
+],
+
+fr:[
+"Quelle a été ta première impression de moi ?",
+"Étais-tu nerveux avant le rendez-vous ?"
 ]
 
-const dateDare = [
+}
+
+const dateDare = {
+
+ru:[
 "Скажи мне комплимент",
-"Сделай милое селфи"
+"Возьми меня за руку"
+],
+
+en:[
+"Give me a compliment",
+"Hold my hand"
+],
+
+es:[
+"Dime un cumplido",
+"Toma mi mano"
+],
+
+fr:[
+"Fais-moi un compliment",
+"Prends ma main"
 ]
+
+}
