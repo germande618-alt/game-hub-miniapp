1c48cab5a4ff62bd50184b2dc893c39c424ddeb4
germande618-alt <germande618@gmail.com>
2026-03-14 16:07:16 +0200

    Update couple_cards.js

--- games/couple/truthdare/couple_cards.js
+++ games/couple/truthdare/couple_cards.js
@@ -1,9 +1,47 @@
-const coupleTruth = [
-"Что тебе больше всего нравится во мне?",
-"Когда ты понял что любишь меня?"
+const coupleTruth = {
+
+ru:[
+"Когда ты понял что влюбился?",
+"Что тебе больше всего во мне нравится?"
+],
+
+en:[
+"When did you realize you loved me?",
+"What do you like most about me?"
+],
+
+es:[
+"¿Cuándo te diste cuenta de que me amabas?",
+"¿Qué es lo que más te gusta de mí?"
+],
+
+fr:[
+"Quand as-tu compris que tu m'aimais ?",
+"Qu'est-ce que tu préfères chez moi ?"
 ]
 
-const coupleDare = [
+}
+
+const coupleDare = {
+
+ru:[
 "Обними меня на 10 секунд",
-"Скажи три причины почему любишь меня"
+"Поцелуй меня"
+],
+
+en:[
+"Hug me for 10 seconds",
+"Kiss me"
+],
+
+es:[
+"Abrázame durante 10 segundos",
+"Bésame"
+],
+
+fr:[
+"Fais-moi un câlin pendant 10 secondes",
+"Embrasse-moi"
 ]
+
+}
