48d8f81d1a7a24a4d368d09c347ee740a6ce0353
germande618-alt <germande618@gmail.com>
2026-03-16 18:10:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,67 @@
+const WebSocket = require("ws")
 
+const PORT = process.env.PORT || 3000
+
+const wss = new WebSocket.Server({ port: PORT })
+
+let rooms = {}
+
+wss.on("connection", ws => {
+
+    ws.on("message", message => {
+
+        const data = JSON.parse(message)
+
+        // создать комнату
+        if(data.type === "create"){
+            const code = Math.random().toString(36).substring(2,6).toUpperCase()
+
+            rooms[code] = [ws]
+            ws.room = code
+
+            ws.send(JSON.stringify({
+                type:"room_created",
+                code:code
+            }))
+        }
+
+        // войти в комнату
+        if(data.type === "join"){
+            const code = data.code
+
+            if(!rooms[code]){
+                ws.send(JSON.stringify({
+                    type:"error",
+                    message:"room_not_found"
+                }))
+                return
+            }
+
+            rooms[code].push(ws)
+            ws.room = code
+
+            ws.send(JSON.stringify({
+                type:"joined",
+                code:code
+            }))
+        }
+
+        // сообщение в комнату
+        if(data.type === "message"){
+            const room = ws.room
+
+            if(!room) return
+
+            rooms[room].forEach(client=>{
+                if(client.readyState === WebSocket.OPEN){
+                    client.send(JSON.stringify({
+                        type:"message",
+                        text:data.text
+                    }))
+                }
+            })
+        }
+
+    })
+
+})
